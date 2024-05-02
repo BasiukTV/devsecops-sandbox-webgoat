68bccfd7835b75ae5b8ffe56d6eda903421b9ee0
Taras Basiuk <BasiukTV@gmail.com>
2024-05-02 00:47:54 -0400

    Adding the Terraform apply step to the CD pipeline.

--- automation/iac/app_svc/terraform/out.tf
+++ automation/iac/app_svc/terraform/out.tf
@@ -1,1 +1,15 @@
+output "rg_name" {
+  description = "Name of the resouce group where the resources ended up being provisioned."
+  value       = module.app_svc_mod.rg_name
+}
+
+output "app_svc_plan_name" {
+  description = "Name of the provisioned App Service plan."
+  value       = module.app_svc_mod.app_svc_plan_name
+}
+
+output "app_svc_name" {
+  description = "Name of the provisioned App Service app."
+  value       = module.app_svc_mod.app_svc_name
+}
 
