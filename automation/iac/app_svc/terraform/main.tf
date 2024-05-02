fbc97ba1d8572be2e4353b7778e7147e79d7bc52
TB-EIT <taras.basiuk@enhanceit.us>
2024-05-01 20:52:21 -0400

    Fixing the Terraform plan step of the CD pipeline.

--- automation/iac/app_svc/terraform/main.tf
+++ automation/iac/app_svc/terraform/main.tf
@@ -3,14 +3,11 @@ terraform {
   required_providers {
     azurerm = "~> 3.96"
   }
+
+  backend "azurerm" {}
 }
 
 provider "azurerm" {
-  tenant_id       = var.tenant_id
-  subscription_id = var.subscription_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-
   features {}
 }
 
